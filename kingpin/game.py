4c1923adab9366b8e315a91ed6619666efd4c0cf
bramz <brockramz@gmail.com>
2018-12-04 08:54:16 -0500

    Updating

--- kingpin/game.py
+++ kingpin/game.py
@@ -22,48 +22,33 @@ def handle_action():
     action = input("Enter an action (buy/sell/travel/stats): ")
 
     if action == "buy":
-        handle_buying()
+        buying_selling(action)
     elif action == "sell":
-        handle_sales()
+        buying_selling(action)
     elif action == "travel":
         handle_travel()
     elif action == "stats":
-        stats = player.get_stats()
-        print(stats)
+        print(player.get_stats(), player.get_drugs(), player.get_weapons())
                 
 
 def list_drugs() -> dict:
     drugs_list = {
-        'Cannabis': random_price(100, 1001),
-        'Cocaine': random_price(1000, 10001),
-        'Heroin': random_price(10000, 30001),
+        'cannabis': random_price(100, 1001),
+        'cocaine': random_price(1000, 10001),
+        'heroin': random_price(10000, 30001),
     }
     return drugs_list
 
 
 def list_weapons() -> dict:
     weapons_list = {
-        'Pistol': random_price(100, 1001),
-        'Rifle': random_price(1000, 10001),
-        'Shotgun': random_price(1000, 10001),
-        'Sniper': random_price(5000, 20001),
-        'Explosives': random_price(10000, 50001),
+        'pistol': random_price(100, 1001),
+        'rifle': random_price(1000, 10001),
+        'shotgun': random_price(1000, 10001),
+        'sniper': random_price(5000, 20001),
+        'explosives': random_price(10000, 50001),
     }
     return weapons_list
-
-
-def handle_buying():
-    purchase_type  = input("What will you buy (drugs/weapons) ? ")
-    if purchase_type == "drugs":
-        print(list_drugs())
-        drug_type = input("What type of drugs? ")
-    elif purchase_type == "weapons":
-        print(list_weapons())
-
-
-def handle_sales():
-    stats = player.get_stats()
-    print(stats)
 
 
 def handle_travel():
@@ -71,6 +56,94 @@ def handle_travel():
     print(stats)
 
 
+def purchase_type() -> str:
+    ptype = input("What will you buy (drugs/weapons)? ")
+    return ptype
+
+
+def item_type() -> str:
+    dtype = input("What type? ")
+    return dtype
+
+
+def buying_selling(action: str):
+    if action == "buy":
+        buying_items(purchase_type())
+    elif action == "sell":
+        selling_items(purchase_type())
+
+
+def buying_items(ptype: str):
+    if ptype == "drugs":
+        dlist = list_drugs()
+        print(dlist)
+
+        itype = item_type()
+        player_cash = player.get_cash()
+        price = dlist[itype][1:]
+        quantity = receive_units()
+        total_cost = determine_total_cost(int(price), int(quantity))
+        print(check_buy_total(quantity, itype, price, total_cost, player_cash))
+    elif ptype == "weapons":
+        wlist = list_weapons()
+        print(wlist)
+
+        itype = item_type()
+        player_cash = player.get_cash()
+        price = wlist[itype][1:]
+        quantity = receive_units()
+        total_cost = determine_total_cost(int(price), int(quantity))
+        print(check_buy_total(quantity, itype, price, total_cost, player_cash))
+
+        
+def selling_items(ptype: str):
+    if ptype == "drugs":
+        dlist = list_drugs()
+        print(dlist)
+        
+        itype = item_type()
+        player_total = player.drugs[itype]
+        price = dlist[itype][1:]
+        quantity = receive_units()
+        total_cost = determine_total_cost(int(price), int(quantity))
+        print(check_sales_total(quantity, itype, price, total_cost, player_total))
+    elif ptype == "weapons":
+        wlist = list_weapons()
+        print(wlist)
+
+        itype = item_type()
+        player_cash = player.get_cash()
+        price = wlist[itype][1:]
+        quantity = receive_units()
+        total_cost = determine_total_cost(int(price), int(quantity))
+        print(check_sales_total(quantity, itype, price, total_cost, player_cash))
+
+
+def check_buy_total(quantity: int, item: str, price: int, total_cost: int, player_cash: int) -> str:
+    if int(total_cost) <= int(player_cash):
+        player.receive_items(item, int(quantity))
+        return "Purchasing {quantity} units of {item} at ${price} for ${total_cost}".format(quantity=quantity, price=price, total_cost=total_cost, item=item)
+    else:
+        return "You do not have enough money."
+
+
+def check_sales_total(quantity: int, item: str, price: int, total_cost: int, player_total: int) ->str:
+    if int(player_total) >= int(quantity):
+        player.expell_items(item, int(quantity))
+        return "Selling {quantity} units of {item} at ${price} for ${total_cost}".format(quantity=quantity, item=item, price=price, total_cost=total_cost)
+    else:
+        return "You do not have {quantity} units of {item}".format(quantity=quantity, item=item)
+
+
 def random_price(price_rangex, price_rangey) -> str:
     return "$%i" % random.randint(price_rangex, price_rangey)
 
+
+def receive_units() -> int:
+    quantity = input("How many units? ")
+    return quantity
+
+
+def determine_total_cost(price: int, quantity: int) -> int:
+    total = price * quantity
+    return total
--- kingpin/player.py
+++ kingpin/player.py
@@ -1,31 +1,31 @@
-# user package
+
 
 class Player:
     def __init__(self, name):
         self.name = name
         self.stats = {
-            'Cash': 250,
-            'Properties': 1,
-            'Cars': 0,
-            'Rank': 0,
+            'cash': 1000,
+            'properties': 1,
+            'cars': 0,
+            'rank': 0,
         }
 
         self.drugs = {
-            'Cannabis': 1,
-            'Cocaine': 0,
-            'Heroin': 0,
+            'cannabis': 1,
+            'cocaine': 0,
+            'heroin': 0,
         }
 
         self.weapons = {
-            'Pistols': 1,
-            'Rifles': 0,
-            'Shotguns': 0,
-            'Snipers': 0,
-            'Explosives': 0,
+            'pistol': 1,
+            'rifle': 0,
+            'shotgun': 0,
+            'sniper': 0,
+            'explosives': 0,
         }
 
 
-    def get_stats(self) -> str:
+    def get_stats(self) -> dict:
         return self.stats
 
     
@@ -35,6 +35,39 @@ class Player:
     
     def get_weapons(self) -> dict:
         return self.weapons
+
+
+    def get_cash(self) -> str:
+        return self.stats['cash']
+
+
+    def get_drug_quantity(self, drug) -> int:
+        return int(self.drugs[drug])
+
+
+    def get_weapon_quantity(self, weapon) -> int:
+        return int(self.weapons[weapon])
+
+
+    def receive_items(self, item: str, amount: int) -> str:
+        if item in self.drugs:
+            total = int(self.get_drug_quantity(item)) + amount
+            self.drugs[item] = total
+            return self.drugs[item]
+        elif item in self.weapons:
+            total = int(self.get_weapon_quantity(item)) + amount
+            self.weapons[item] = total
+            return self.weapons[item]
+
+
+    def update_cash(self, amount: int, operation: str) -> str:
+        cash = self.get_cash()
+        if operation == "add":
+            new_cash = int(cash) + int(amount)
+            self.stats['cash'] = new_cash
+        elif operation == "sub":
+            new_cash = int(cash) - int(amount)
+            self.stats['cash'] = new_cash
 
 
 def create_player(name) -> Player:
