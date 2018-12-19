0e4cb9ac41cf0af811f905b07f69af0a130c5ddb
bramz <brockramz@gmail.com>
2018-12-18 22:46:06 -0500

    Clean up nested lists

--- engine/story.py
+++ engine/story.py
@@ -10,19 +10,19 @@ class Story:
     def get_levels(self) -> List:
         self.levels = list(self.data['levels'])
 
-        return [[y for y in x] for x in self.levels]
+        return self.levels
 
     def get_roles(self, level: str) -> List:
-        self.roles = list(self.data['levels'][0][level][0]['roles'])
+        self.roles = list(self.data['levels'][level]['roles'])
 
-        return [[y for y in x] for x in self.roles]
+        return self.roles
 
 
 class Script:
     def __init__(self):
         self.script: str 
 
-    def read_story_script(self, script: str) -> Story:
+    def read_from_json(self, script: str) -> Story:
         self.script = script
         try:
             with open(self.script, 'r') as fh:
@@ -33,37 +33,36 @@ class Script:
 
 
 def get_spawns(story: Story, level: str, role: str) -> List:
-    spawns = story.data['levels'][0][level][0]['roles'][0][role][0]['spawns']
-    return list([[y for y in x] for x in spawns])
+    spawns = story.data['levels'][level]['roles'][role]['spawns']
+    return list(spawns)
 
 
 def spawn_option(story: Story, level: str, role: str, option: str) -> List[Dict]:
-    options = story.data['levels'][0][level][0]['roles'][0][role][0]['spawns'][0][option]
+    options = story.data['levels'][level]['roles'][role]['spawns'][option]
     return options
 
 
-def random_spawn(spawns: List) -> str:
-    return random.choice([x for x in spawns[0] if not x.startswith('@$\spawn') and not x.startswith('option')])
+def random_spawn(spawns: Dict) -> str:
+    return random.choice([x for x in spawns if not x.startswith('@$\spawn') and not x.startswith('option')])
 
 
-def random_option(spawns: List) -> str:
-    return random.choice([x for x in spawns[0] if not x.startswith('@$\option') and not x.startswith('spawn')])
+def random_option(spawns: Dict) -> str:
+    return random.choice([x for x in spawns if not x.startswith('@$\option') and not x.startswith('spawn')])
 
 
 def parse_options(options: List[Dict], opt: str) -> str:
-    return options[0][opt]
+    return options[opt]
 
 """ Usage examples """ 
 """
 script = Script()
-s = script.read_story_script('script.json')
+s = script.read_from_json('script.json')
 
 levels = s.get_levels()
-roles = s.get_roles('level0')
-spawns = get_spawns(s, 'level0', 'slave')
-spawn_msg = spawn_option(s, 'level0', 'slave', random_spawn(spawns))
-options = spawn_option(s, 'level0', 'slave', random_option(spawns))
+roles = s.get_roles('level1')
+spawns = get_spawns(s, 'level1', 'craftsman')
+spawn_msg = spawn_option(s, 'level1', 'craftsman', random_spawn(spawns))
+options = spawn_option(s, 'level1', 'craftsman', random_option(spawns))
 
 print(levels, roles, spawns, options, spawn_msg, parse_options(options, '2'))
 """
-
